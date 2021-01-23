20ce8443816af742188b217109d77b5b654f887a
Shubham <shubhamkumar876@gmail.com>
2021-01-23 18:35:05 +0530

    complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { Ng2OrderModule } from 'ng2-order-pipe';
@@ -13,10 +13,12 @@ import { ProductService } from './services/product.service';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 import { RatingModule } from 'ng-starrating';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CheckoutComponent } from './components/checkout/checkout.component';
 
 
 const routes: Routes = [
   
+  {path:'checkout', component:CheckoutComponent},
   {path:'cart', component:CartDetailsComponent},
   {path: 'books', component: BookListComponent},
   {path: '', redirectTo: '/books', pathMatch: 'full'},
@@ -27,7 +29,8 @@ const routes: Routes = [
   declarations: [
     AppComponent,
     BookListComponent,
-    CartDetailsComponent
+    CartDetailsComponent,
+    CheckoutComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
@@ -38,7 +41,8 @@ const routes: Routes = [
     Ng2SearchPipeModule,
     FormsModule,
     RatingModule,
-    NgbModule
+    NgbModule,
+    ReactiveFormsModule
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
---
+++ src/app/components/checkout/checkout.component.ts
@@ -0,0 +1,268 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CartService } from 'src/app/services/cart.service';
+import { CheckoutFormServiceService } from 'src/app/services/checkout-form-service.service';
+import { ShopValidators } from 'src/app/validators/shop-validators';
+
+@Component({
+  selector: 'app-checkout',
+  templateUrl: './checkout.component.html',
+  styleUrls: ['./checkout.component.css']
+})
+export class CheckoutComponent implements OnInit {
+
+  checkoutFormGroup!: FormGroup;
+
+  totalPrice: number = 0.0;
+  totalQuantity: number = 0;
+
+  creditCardYears: number[] = [];
+  creditCardMonths: number[] = [];
+
+
+
+
+  constructor(private formBuilder: FormBuilder,
+              private checkoutFormService: CheckoutFormServiceService,
+              private cartService: CartService,
+              private router: Router) { }
+
+  ngOnInit(): void {
+
+    this.reviewCartstatus();
+    this.cartService.computeCartTotals();
+
+    ////////////////////////////////////////
+    this.checkoutFormGroup = this.formBuilder.group ({
+      customer: this.formBuilder.group({
+        firstName: new FormControl('', [Validators.required,Validators.minLength(3), 
+                                    ShopValidators.notOnlyWhitespace]),
+        lastName: new FormControl('', [Validators.required,Validators.minLength(3),
+                                     ShopValidators.notOnlyWhitespace]),
+        email: new FormControl('', 
+                                [Validators.required,
+                                  Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),])
+      }),
+      shippingAddress: this.formBuilder.group ({
+        street: new FormControl('', [Validators.required,Validators.minLength(2), 
+                                ShopValidators.notOnlyWhitespace]),
+        city: new FormControl('', [Validators.required,Validators.minLength(2), 
+                                ShopValidators.notOnlyWhitespace]),
+        state:new FormControl('', [Validators.required]),
+        country: new FormControl('', [Validators.required]),
+        zipCode: new FormControl('', [Validators.required,Validators.minLength(6), 
+                                  ShopValidators.notOnlyWhitespace])
+      }),
+      billingAddress: this.formBuilder.group ({
+        street: new FormControl('', [Validators.required,Validators.minLength(2), 
+                                  ShopValidators.notOnlyWhitespace]),
+        city: new FormControl('', [Validators.required,Validators.minLength(2), 
+                                ShopValidators.notOnlyWhitespace]),
+        state:new FormControl('', [Validators.required]),
+        country: new FormControl('', [Validators.required]),
+        zipCode: new FormControl('', [Validators.required,Validators.minLength(6), 
+                                    ShopValidators.notOnlyWhitespace])
+      }),
+      creditCard: this.formBuilder.group ({
+        cardType: new FormControl('', [Validators.required]),
+        nameOnCard:  new FormControl('', [Validators.required,Validators.minLength(3), 
+                                        ShopValidators.notOnlyWhitespace]),
+        cardNumber: new FormControl('', [Validators.required, Validators.pattern('[0-9]{16}')]),
+        securityCode: new FormControl('', [Validators.required, Validators.pattern('[0-9]{3}')]),
+        expirationMonth: [''],
+        expirationYear: ['']
+      }),
+    });
+
+    // populate credit card months
+    const startMonth: number = new Date().getMonth() + 1;
+    console.log("start month = " +  startMonth);
+
+    this.checkoutFormService.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        console.log("Retrieved credit card months: " + JSON.stringify(data));
+        this.creditCardMonths = data;
+      }
+    );
+
+    // populate credit card years
+    this.checkoutFormService.getCreditCardYears().subscribe(
+      data => {
+        console.log("Retrieved credit card years: " + JSON.stringify(data));
+        this.creditCardYears = data;
+      }
+    );
+
+
+  }
+
+  get firstName() {
+    return this.checkoutFormGroup.get('customer.firstName');
+  }
+
+  get lastName() {
+    return this.checkoutFormGroup.get('customer.lastName');
+  }
+
+  get email() {
+    return this.checkoutFormGroup.get('customer.email');
+  }
+
+  //////////////////////////////////////////////////
+
+  get shippingAddressStreet() {
+    return this.checkoutFormGroup.get('shippingAddress.street');
+  }
+
+  get shippingAddressCity() {
+    return this.checkoutFormGroup.get('shippingAddress.city');
+  }
+
+  get shippingAddressCountry() {
+    return this.checkoutFormGroup.get('shippingAddress.country');
+  }
+
+  get shippingAddressState() {
+    return this.checkoutFormGroup.get('shippingAddress.state');
+  }
+
+  get shippingAddressZipCode() {
+    return this.checkoutFormGroup.get('shippingAddress.zipCode');
+  }
+
+  /////////////////////////////////////////////////////////
+
+  get billingAddressStreet() {
+    return this.checkoutFormGroup.get('billingAddress.street');
+  }
+
+  get billingAddressCity() {
+    return this.checkoutFormGroup.get('billingAddress.city');
+  }
+
+  get billingAddressCountry() {
+    return this.checkoutFormGroup.get('billingAddress.country');
+  }
+
+  get billingAddressState() {
+    return this.checkoutFormGroup.get('billingAddress.state');
+  }
+
+  get billingAddressZipCode() {
+    return this.checkoutFormGroup.get('billingAddress.zipCode');
+  }
+
+  /////////////////////////////////////////////////////////
+
+  get creditCardType() {
+    return this.checkoutFormGroup.get('creditCard.cardType');
+  }
+
+  get creditCardNameOnCard() {
+    return this.checkoutFormGroup.get('creditCard.nameOnCard');
+  }
+
+  get creditCardNumber() {
+    return this.checkoutFormGroup.get('creditCard.cardNumber');
+  }
+
+  get creditCardSecurityCode() {
+    return this.checkoutFormGroup.get('creditCard.securityCode');
+  }
+
+  /////////////////////////////////////////////////////////
+
+
+  reviewCartstatus() {
+
+    // subscribe to the cart totalPrice
+    this.cartService.totalPrice.subscribe(
+      data => this.totalPrice = data
+    );
+
+
+      // subscribe to the cart totalQuantity
+    this.cartService.totalQuantity.subscribe(
+      data => this.totalQuantity = data
+    );  
+
+  }
+
+
+  onSubmit() {
+
+    if (this.checkoutFormGroup.invalid) {
+      this.checkoutFormGroup.markAllAsTouched();
+      return;
+    }
+
+    
+    
+            alert(`Your Order has been received`);
+
+            // reset the cart
+            this.resetCart();
+        
+  }
+
+
+
+  resetCart() {
+
+      //reset the cart data
+      this.cartService.cartItems = [];
+      this.cartService.totalPrice.next(0);
+      this.cartService.totalQuantity.next(0);
+
+      //reset the form data
+      this.checkoutFormGroup.reset();
+
+      //navigate back to the products page
+      this.router.navigateByUrl("/books");
+
+  }
+
+  copyShippingAddressToBillingAddress(event: any) {
+
+    if (event.target.checked) {
+      this.checkoutFormGroup.controls.billingAddress
+            .setValue(this.checkoutFormGroup.controls.shippingAddress.value);
+    }
+
+    else {
+      this.checkoutFormGroup.controls.billingAddress.reset();
+
+    }
+
+  }
+
+  handleMonthsAndYears() {
+
+    const creditCardFormGroup = this.checkoutFormGroup.get('creditCard')!;
+
+    const currentYear: number = new Date().getFullYear();
+    const selectedYear: number = Number(creditCardFormGroup.value.expirationYear);
+
+    // if the current year equals to the selected year, the start with the current month
+
+    let startMonth: number = 1;
+
+    if (currentYear == selectedYear){
+      startMonth = new Date().getMonth() + 1; // 0 based month system in rxjs
+    }
+    else {
+      startMonth = 1;
+    }
+
+    this.checkoutFormService.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        console.log("Retrieved credit months: " + JSON.stringify(data));
+        this.creditCardMonths = data;
+      }
+    );
+
+  }
+
+
+}
